611295800aac394f528f923095c8768d04994355
tayloraj10 <taylor.alan.johnson@gmail.com>
2018-06-30 22:28:32 -0400

    Add project files.

---
+++ GeekQuiz/Scripts/app/quiz-controller.js
@@ -0,0 +1,45 @@
+﻿angular.module('QuizApp', [])
+    .controller('QuizCtrl', function ($scope, $http) {
+        $scope.answered = false;
+        $scope.title = "loading questions...";
+        $scope.options = [];
+        $scope.correctAnswer = false;
+        $scope.working = false;
+
+        $scope.answer = function () {
+            return $scope.correctAnswer ? 'correct' : 'incorrect';
+        };
+
+        $scope.nextQuestion = function () {
+            $scope.working = false;
+            $scope.answered = false;
+            $scope.title = 'loading question...';
+            $scope.options = [];
+
+            $http.get("/api/trivia").then(function (data) {
+                data = data.data;
+                $scope.options = data.options;
+                $scope.title = data.title;
+                $scope.answered = false;
+                $scope.working = false;
+            }, function (error) {
+                $scope.title = "Oops... something went wrong";
+                $scope.working = false;
+            });
+        };
+
+        $scope.sendAnswer = function (option) {
+            $scope.working = true;
+            $scope.answered = true;
+
+            $http.post('/api/trivia', { 'questionId': option.questionId, 'optionId': option.id })
+                .then(function (data) {
+                    data = data.data
+                    $scope.correctAnswer = (data === true);
+                    $scope.working = false;
+                }, function (error) {
+                    $scope.title = "Oops... something went wrong";
+                    $scope.working = false;
+                });
+        };
+    });
